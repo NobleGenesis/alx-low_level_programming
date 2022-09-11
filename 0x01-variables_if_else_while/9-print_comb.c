e416c9f31868b27eec0d9b41b3e96244583d053e
NobleGenesis <igbangamaha95@gmail.com>
2022-09-11 10:07:15 -0700

    edited

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,18 +7,18 @@
  */
 int main(void)
 {
-		int number;
+	int number;
 
-		for (number = 0; nubmer <= 9; number++)
-		{
-		putchar(number + '0');
-		if (number < 9)
-		{
-		putchar(',');
-		putchar(' ');
-		}
-		}
-		putchar('\n');
+	for (number = 0; nubmer <= 9; number++)
+	{
+	putchar(number + '0');
+	if (number < 9)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	putchar('\n');
 
 	return (0);
 }
