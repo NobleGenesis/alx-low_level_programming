b7a0596883e38495b8b446588db3b3973346e09c
NobleGenesis <igbangamaha95@gmail.com>
2022-09-11 09:31:58 -0700

    Edited a text

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,15 +7,14 @@
  */
 int main(void)
 {
-		char digit;
+	char digit;
 
-		for (digit = '0'; digit <= '9'; digit++)
+	for (digit = '0'; digit <= '9'; digit++)
 		putchar(digit);
 
-		for (digit = 'a'; digit <= 'f'; digit++)
+	for (digit = 'a'; digit <= 'f'; digit++)
 		putchar(digit);
 
-		putchar('\n');
-	
+	putchar('\n');	
 	return (0);
 }
