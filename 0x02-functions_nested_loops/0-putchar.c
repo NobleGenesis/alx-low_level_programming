ee4b97e7916ea5983000b2fea0c0533aa25d03bb
NobleGenesis <igbangamaha95@gmail.com>
2022-09-14 21:23:43 -0700

    Checked and edited for potential errors

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,14 @@
-#include <main.h>
+#include "main.h"
 
 /**
- * * main - Entry point
+ * main - Entry point
  *
- * return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	_putchar('_')'
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
