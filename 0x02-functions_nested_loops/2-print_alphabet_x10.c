035ca6d7d863a832c7868b81affdb60813758f4e
NobleGenesis <igbangamaha95@gmail.com>
2022-09-19 03:13:15 -0700

    Edited

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,6 +2,7 @@
 
 /**
  * print_alphabet_x10 - repeats the print_alphabet 10 times.
+ *
  * Return: Nothing.
  */
 
@@ -10,12 +11,14 @@ void print_alphabet_x10(void)
 	int i;
 	char c;
 
-	for (i = 0; i < 10; ++i)
+	for (i = 0; i < 10; i++)
 	{
 		for (c = 'a'; c <= 'z'; c++)
 		{
 			putchar(c);
 		}
+
 		_putchar('\n');
 	}
+	return (0);
 }
