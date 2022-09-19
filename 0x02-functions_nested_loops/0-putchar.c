1bb44ceb97e45b3cd0f58abb6cbca6f73e70fa32
NobleGenesis <igbangamaha95@gmail.com>
2022-09-19 01:23:09 -0700

    edited

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,15 @@
 #include "main.h"
 
 /**
- * main - prints the string "Putchar" from a character array
+ * main - prints the string 'Putchar' from a character array
  *
  * Return: 0 on Success
  */
 
 int main(void)
 {
+	char _putchar;
+
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
