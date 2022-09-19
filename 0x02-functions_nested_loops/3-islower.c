d90964feff1bb28b514485fdb6f5cbf8edad9013
NobleGenesis <igbangamaha95@gmail.com>
2022-09-19 04:12:14 -0700

    Edited

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,7 +6,7 @@
  * @c: character to test.
  * Return: 1 if it is a lowercase letter, 0 if it is not a lowercase letter.
  */
-int _islower(int C)
+int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
 		return (1);
