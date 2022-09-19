a5c490a7fec003fcba2ab498592d183a01a7780a
NobleGenesis <igbangamaha95@gmail.com>
2022-09-19 00:51:38 -0700

    I eddited my command

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,22 +2,19 @@
 #include "main.h"
 
 /**
- * main - Entry point
- *
+ * main - prints the string "Putchar" from a character array
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	char c[] = "Putchar";
+	int i;
 
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
 	return (0);
 }
