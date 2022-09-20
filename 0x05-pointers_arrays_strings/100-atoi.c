7057cd525fa4b4ceed7d2ba8ac82866a36311644
NobleGenesis <igbangamaha95@gmail.com>
2022-09-19 23:21:26 -0700

    I edited the function

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,39 +8,26 @@
  */
 int _atoi(char *s)
 {
-	int i, d, n, len, f, digit;
+	short boolean;
+	int i, minus, result;
 
-	i = 0;
-	d = 0;
-	n = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
+	i = minus = result = boolean = 0;
+	minus = -1;
 
-	while (s[len] != '\0')
-		len++;
-
-	while (i < len && f == 0)
+	while (s[i] != '\0')
 	{
 		if (s[i] == '-')
-			++d;
-
+			minus *= -1;
 		if (s[i] >= '0' && s[i] <= '9')
 		{
-			digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
-			n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			f = 0;
+			result *= 10;
+			result -= (s[i] - '0');
+			boolean = 1;
 		}
-		i++
+		else if (boolean == 1)
+			break;
+		i++;
 	}
-
-	if (f == 0)
-		return (0);
-
-	return (n);
+	result *= minus;
+	return (result);
 }
